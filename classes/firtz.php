2e65a9dc7f803066e6970ba9579fe972dcca29fa
Christian Bednarek <eazy@eazy.amigager.de>
2013-02-20 17:25:36 +0100

    extension class

--- classes/firtz.php
+++ classes/firtz.php
@@ -1,9 +1,16 @@
 <?php
 
+	/* 
+		application class 
+		some globals and extensions
+
+	*/	
+	
 	class firtz {
 	
 	
 		public $data = array();
+		public $extensions = array();
 		
 		function __construct() {
 		
@@ -12,10 +19,43 @@
 			$this->main = $main;
 			$main->set('firtz',$this);
 			$this->BASEURL ="http://".$main->get('HOST');
-			$this->BASEPATH = $_SERVER['DOCUMENT_ROOT'].dirname($_SERVER['SCRIPT_NAME']);
-			#$main->set('BASEURL',$this->BASEURL)."/";
+			$this->BASEPATH = $_SERVER['DOCUMENT_ROOT'];
+			if (dirname($_SERVER['SCRIPT_NAME']) != "/") $this->BASEPATH.=dirname($_SERVER['SCRIPT_NAME']);
+
 			$main->set('BASEPATH',$this->BASEPATH);
+			
 		}
+	
+		
+
+		function loadAllTheExtensions() {
+		
+			$main = $this->main;
+
+			if (!file_exists($main->get('BASEPATH').'ext/')) return;
+			
+			foreach (glob($main->get('BASEPATH').'ext/*',GLOB_ONLYDIR) as $dir) {
+				
+				$extension = new extension ($main,$dir);
+				
+				if ($extension===false) {
+					die("failed to load extension at $dir");
+				} else {
+					foreach ($this->extensions as $ext) {
+						if ($ext->slug == $extension->slug) {
+							die("failed to load extension at $dir - slug $this->slug already registered!");
+						}
+					}
+					$this->extensions[$extension->slug]=$extension;
+						
+				}
+			}
+		
+		}
+		
+		
+	
+	
 	}
 
 ?>
