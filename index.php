152e9874901cca7550cd0b137dd482ead3abb4b0
Christian Bednarek <eazy@eazy.amigager.de>
2013-02-26 10:24:55 +0100

    dienstagvormittagcommit

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ $main->set('FEEDDIR','./feeds');
 
 $main->set('UI','templates/');
 $main->set('version',0);
-$main->set('revision',4);
+$main->set('revision',5);
 $main->set('generator','firtz feed generator v'.$main->get('version').".".$main->get('revision'));
 $main->set('pager','');
 $main->set('BASEURL',"http://".$main->get('HOST').dirname($_SERVER['SCRIPT_NAME']));
