bb6fc46af953baa4ad29fcd731ef66d6fd62570e
eazyliving <eazy@eazy-living.de>
2013-04-04 16:05:30 +0200

    small changes to archive page

--- dict/de.php
+++ dict/de.php
@@ -1,7 +1,7 @@
 <?php
 return array(
-	'dict_subscribe'=>'abonnieren',
-	'dict_pubdate'=>'{0,date} {0,time}',
+	'dict_subscribe'=>'Abonnieren',
+	'dict_pubdate'=>'{0,date}',
 	'dict_showcomments'=>'Kommentare zeigen',
 	'dict_license'=>'Lizenz',
 	'dict_older'=>'&auml;lter',
--- dict/en.php
+++ dict/en.php
@@ -2,7 +2,7 @@
 return array(
     
 	'dict_subscribe'=>'subscribe',
-    'dict_pubdate'=>'{0,date} {0,time}',
+    'dict_pubdate'=>'{0,date}',
 	'dict_showcomments'=>'show comments',
 	'dict_license'=>'Lizenz',
 	'dict_older'=>'older',
