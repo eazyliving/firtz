a9e82a03587d57c870addad9012602aab5055586
eazyliving <eazy@eazy-living.de>
2013-04-04 16:09:35 +0200

    small changes to archive page

--- dict/de.php
+++ dict/de.php
@@ -2,6 +2,7 @@
 return array(
 	'dict_subscribe'=>'Abonnieren',
 	'dict_pubdate'=>'{0,date}',
+	'dict_duration'=>'Dauer',
 	'dict_showcomments'=>'Kommentare zeigen',
 	'dict_license'=>'Lizenz',
 	'dict_older'=>'&auml;lter',
--- dict/en.php
+++ dict/en.php
@@ -3,6 +3,7 @@ return array(
     
 	'dict_subscribe'=>'subscribe',
     'dict_pubdate'=>'{0,date}',
+	'dict_duration'=>'duration',
 	'dict_showcomments'=>'show comments',
 	'dict_license'=>'Lizenz',
 	'dict_older'=>'older',
