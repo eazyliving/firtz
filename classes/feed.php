3769c7c0536179d6b4a36b99b9043adc818e3298
eazyliving <eazy@eazy-living.de>
2013-04-22 13:47:12 +0200

    fixed <link> in feed for dedicated feeds

--- classes/feed.php
+++ classes/feed.php
@@ -103,7 +103,7 @@
 			$attr['categories']=$categories;
 			
 			$attr['slug']=$slug;
-			$attr['link'] = $main->get('REALM')."/show";
+			$attr['link'] = $main->get('BASEURL').$attr['slug'].'/show';
 			$attr['self'] = $main->get('REALM');
 			
 			if ($attr['cloneurl']!='' && substr($attr['cloneurl'],-1)!='/') $attr['cloneurl'].='/';
