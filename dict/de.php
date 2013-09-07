f5c0acb1f0d63042d77532aaefc83faccb89f549
Simon Waldherr <git@simon.waldherr.eu>
2013-09-07 16:23:50 +0200

    jQuery update to 1.10.2 PWP update to 2.0.15 whitespace beautification firefox xss securefix

--- dict/de.php
+++ dict/de.php
@@ -4,11 +4,10 @@ return array(
 	'dict_pubdate'=>'{0,date}',
 	'dict_duration'=>'Dauer',
 	'dict_showcomments'=>'Kommentare zeigen',
-    'dict_show_adn_thread'=>'Thread auf App.net anzeigen',
+	'dict_show_adn_thread'=>'Thread auf App.net anzeigen',
 	'dict_license'=>'Lizenz',
 	'dict_older'=>'&auml;lter',
 	'dict_newer'=>'neuer',
-	'dict_reply'=>'antworten',
-    
+	'dict_reply'=>'antworten'
 );
 ?>
