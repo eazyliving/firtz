c7c7271ec4cad61765baf0f5a84c6a2d5c70c1e2
eazyliving <eazy@eazy-living.de>
2013-09-13 11:39:29 +0200

    updated

--- dict/de.php
+++ dict/de.php
@@ -3,7 +3,7 @@ return array(
 	'dict_subscribe'=>'Abonnieren',
 	'dict_pubdate'=>'{0,date}',
 	'dict_duration'=>'Dauer',
-	'dict_showcomments'=>'Kommentare zeigen',
+	'dict_showcomments'=>'Kommentare zeigen oder schreiben',
 	'dict_show_adn_thread'=>'Thread auf App.net anzeigen',
 	'dict_license'=>'Lizenz',
 	'dict_older'=>'&auml;lter',
