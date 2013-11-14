e0767d6cc306ee75e798f132c05a8586d01d4fbd
eazyliving <eazy@eazy-living.de>
2013-11-14 10:03:34 +0100

    fixed html-bug inside subtitle

--- classes/feed.php
+++ classes/feed.php
@@ -350,7 +350,7 @@
 				# no feed image? take the first found episode image...
 				if ($this->attr['image']=="" && $episode->item['image']!="") $this->attr['image']=$episode->item['image'];
 				$episode->item['article'] =  $this->markdown->convert(strip_tags($episode->item['article']));
-				$episode->item['description'] =  $this->markdown->convert(strip_tags($episode->item['description']));
+				$episode->item['description'] =  strip_tags($this->markdown->convert($episode->item['description']),"<a>");
 				$episode->item['summary'] =  strip_tags($episode->item['article']);
 				
 				foreach ($firtz->extensions as $extslug => $ext) {
