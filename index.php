d9fff3930ea1cbf3fcb1d488cdc13a38549f3d1c
Christian Bednarek <eazy@eazy-living.de>
2015-01-09 14:26:37 +0100

    go on

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ $main->set('FEEDDIR','./feeds');
 $main->set('UI','templates/');
 $main->set('templatepath','templates/');
 $main->set('version',1);
-$main->set('revision',4);
+$main->set('revision',5);
 $main->set('generator','firtz podcast publisher v'.$main->get('version').".".$main->get('revision'));
 $main->set('pager','');
 $main->set('BASEURL',"http://".str_replace("/","",$main->get('HOST')).dirname($_SERVER['SCRIPT_NAME']));
