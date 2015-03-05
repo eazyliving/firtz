523448397b8fac51e8483826c0fe106d2c3322fc
Christian Bednarek <eazy@eazy-living.de>
2015-03-05 20:15:26 +0100

    podlove webplayer 2.0.17

--- src/pwp/static/podlove-web-player.js
+++ src/pwp/static/podlove-web-player.js
@@ -1,9 +1,18 @@
 /*
  * ===========================================
- * Podlove Web Player v2.0.15
+ * Podlove Web Player v2.0.17
  * Licensed under The BSD 2-Clause License
  * http://opensource.org/licenses/BSD-2-Clause
  * ===========================================
+ * Copyright (c) 2013, Gerrit van Aaken (https://github.com/gerritvanaaken/), Simon Waldherr (https://github.com/simonwaldherr/), Frank Hase (https://github.com/Kambfhase/), Eric Teubert (https://github.com/eteubert/) and others (https://github.com/podlove/podlove-web-player/contributors)
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+ *
+ * - Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+ * - Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
 
@@ -250,14 +259,13 @@ if (typeof String.prototype.trim !== 'function') {
         // required (minutes : seconds)
         part = zeroFill(minutes, 2) + ':' + zeroFill(seconds, 2);
         hours = zeroFill(hours, 2);
-        hours = hours === '00' && !forceHours ? '' : hours + ':';
-        milliseconds = milliseconds ? '.' + zeroFill(milliseconds, 3) : '';
+        hours = hours === '00' && !forceHours ? '' : hours + ':';        
       } else {
         part = hours ? zeroFill(minutes, 2) : minutes.toString();
         part += ':' + zeroFill(seconds, 2);
         hours = hours ? hours + ':' : '';
-        milliseconds = milliseconds ? '.' + milliseconds : '';
       }
+      milliseconds = milliseconds ? '.' + zeroFill(milliseconds, 3) : '';
       return hours + part + milliseconds;
     }
     if (times[1] > 0 && times[1] < 9999999 && times[0] < times[1]) {
@@ -584,8 +592,8 @@ if (typeof String.prototype.trim !== 'function') {
     jqPlayer.on('error', function () {
       if ($(this).attr('src')) {
         $(this).removeAttr('src');
-      } else {
-        $(this).children().first().remove();
+      } else if($(this).children('source').length) {
+        $(this).children('source').first().remove();
       }
     });
     /**
